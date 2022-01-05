b2226f6e06c53f410dada8121748dfbb198833de
KhantThuHtet <khantthu7007@gmail.com>
2022-01-05 21:37:24 +0630

    add item page done

---
+++ js/add_item.js
@@ -0,0 +1,11 @@
+let hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
+let sidebar = document.getElementsByClassName("sidebar")[0];
+let closeMenu = document.getElementsByClassName("close-menu")[0];
+
+hamburgerMenu.addEventListener("click", () => {
+  sidebar.style.left = "0";
+});
+
+closeMenu.addEventListener("click", () => {
+  sidebar.style.left = "-100%";
+});
